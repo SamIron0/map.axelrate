ddb90279acba01bf3821865ffda3d288bf1eaf99
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-09-25 11:37:37 -0500

    s

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main classNameName="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="p-6">
         <h1 className="text-4xl font-bold mb-4">WHO WE ARE</h1>
         <p className="text-lg mb-6">
