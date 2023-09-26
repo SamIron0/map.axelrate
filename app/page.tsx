27d525c177b077d72efcfe68fecf20fe900d1b91
sam <ironkwec@myumanitoba.ca>
2023-09-26 10:30:22 -0500

    df

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="p-6">
-        <h1 className="text-4xl font-bold mb-4">WHO WE ARE</h1>
+        <h1 className="text-4xl font-bold mb-4">OUR GOAL</h1>
         <p className="text-lg mb-6">
           We are a private non-governmental organzation dedicated to rapidly
           accelerating the industrialization of Nigeria and then Africa.
@@ -24,6 +24,17 @@ export default function Home() {
           Democracy because power corrupts and absolute power corrupts
           absolutely.
         </p>
+        <h1 className="text-4xl font-bold mb-4">WHAT WE DO</h1>
+        <p className="text-lg mb-3">
+          We set up systems that operate in the backend to serve as foundation
+          on which individuals and organizations can buuld off of.
+        </p>
+        <h1 className="text-4xl font-bold mb-4">OUR IDEAS</h1>
+        <p className="text-lg mb-3">
+          Solar farm in ...
+        </p>
+        <h1 className="text-4xl font-bold mb-4">WE ARE LEARNERS</h1>
+        <p className="text-lg mb-3">Recommended reading list.</p>
       </div>
     </main>
   );
