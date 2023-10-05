dddbcb6cce4eb24ecad71c88a69fc6145c56f24b
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-10-05 20:33:17 +0000

    d

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="">
+    <main className="height:500px">
       <div id="map"></div>
       {/* Rest of your code */}
     </main>
