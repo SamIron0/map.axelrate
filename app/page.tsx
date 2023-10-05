887bffa5c15035159da5fcb66326bab5a4509b83
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-10-05 20:54:30 +0000

    s

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   useEffect(() => {
     if (typeof window !== "undefined") {
       let L = require('leaflet');
-      let map = L.map('map').setView([9.082, 8.6753], 6);
+      var map = L.map('map').setView([51.505, -0.09], 13);
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -18,7 +18,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="height:500px">
+    <main className="h-500px">
       <div id="map"></div>
       {/* Rest of your code */}
     </main>
