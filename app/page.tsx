fd897584d7da902439316d49c4f1a852d3a1c54d
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-10-05 19:06:00 +0000

    x

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,24 @@
-"use client"
-import Image from "next/image";
-import L, { GeoJSON } from 'leaflet';
-import { useEffect } from 'react';
-import * as nigeriaBorder from "../nigeriaBorder.json";
+import { useEffect, useRef } from 'react';
+
 export default function Home() {
+  const mapRef = useRef(null);
 
-  let map = L.map("map").setView([9.082, 8.6753], 6);
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-  }).addTo(map);
-  var border = L.geoJson(nigeriaBorder).addTo(map);
-  map.fitBounds(border.getBounds());
-
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      let L = require('leaflet');
+      let map = L.map(mapRef.current).setView([9.082, 8.6753], 6);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+      }).addTo(map);
+      var border = L.geoJson(nigeriaBorder as GeoJSON.FeatureCollection).addTo(map);
+      map.fitBounds(border.getBounds());
+    }
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="p-6">
-        <h1 className="text-4xl font-bold mb-4">
-          MY MOTHERLAND, A FAILED STATE
-        </h1>
-        <p className="text-lg mb-6">
-          I planned to start this paper by giving you a brief picture of how we
-          got to this mess reserching
-        </p>
-
-        <br />
-
-      </div>
+      <div id="map" ref={mapRef} style={{ height: "500px", width: "100%" }}></div>
+      {/* Rest of your code */}
     </main>
   );
-
 }
