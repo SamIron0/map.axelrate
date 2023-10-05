2e62427c770441002a2ec7aa26dbd543b79cdd48
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-10-05 21:11:52 +0000

    s

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="">
+    <>
       <div id="map"></div>
       <div>hello</div>
       {/* Rest of your code */}
-    </main>
+    </>
   );
 }
