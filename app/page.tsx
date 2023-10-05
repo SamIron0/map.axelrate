d8c652f73cd70a1416f7a88905a063aa492d81fe
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-10-05 19:50:15 +0000

    e

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <div id="map" ref={mapRef} style={{ height: "500px", width: "100%" }}></div>
       {/* Rest of your code */}
     </main>
