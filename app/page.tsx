4916fb16fac842c7bc59e879d8516df38a192640
sam <ironkwec@myumanitoba.ca>
2023-10-05 12:29:42 -0500

    s

--- app/page.tsx
+++ app/page.tsx
@@ -4,37 +4,26 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="p-6">
-        <h1 className="text-4xl font-bold mb-4">OUR GOAL</h1>
+        <h1 className="text-4xl font-bold mb-4">
+          MY MOTHERLAND, A FAILED STATE
+        </h1>
         <p className="text-lg mb-6">
-          We are a private non-governmental organzation dedicated to rapidly
-          accelerating the industrialization of Nigeria and then Africa.
+          I planned to start this paper by giving you a brief picture of how we
+          got to this mess reserching
         </p>
-        <h1 className="text-4xl font-bold mb-4">GET INVOLVED</h1>
-        <p className="text-lg mb-6">
-          No one is coming to save us, we must take responsibility like every
-          nation that has achieved anything great. Leave your email, we will
-          contact you.
-        </p>
-        <h1 className="text-4xl font-bold mb-4">WHAT WE BELIEVE</h1>
-        <p className="text-lg mb-3">
-          All humans are created equal and have equal rights.
-        </p>
-        <p className="text-lg mb-3">The scientific method.</p>
-        <p className="text-lg mb-6">
-          Democracy because power corrupts and absolute power corrupts
-          absolutely.
-        </p>
-        <h1 className="text-4xl font-bold mb-4">WHAT WE DO</h1>
-        <p className="text-lg mb-3">
-          We set up systems that operate in the backend to serve as foundation
-          on which individuals and organizations can buuld off of.
-        </p>
-        <h1 className="text-4xl font-bold mb-4">OUR IDEAS</h1>
-        <p className="text-lg mb-3">
-          Solar farm in ...
-        </p>
-        <h1 className="text-4xl font-bold mb-4">WE ARE LEARNERS</h1>
-        <p className="text-lg mb-3">Recommended reading list.</p>
+
+        <iframe
+          width="425"
+          height="350"
+          src="https://www.openstreetmap.org/export/embed.html?bbox=0.13183593750000003%2C-0.7031073524364783%2C16.259765625000004%2C20.24158281954221&amp;layer=mapnik"
+          style="border: 1px solid black"
+        ></iframe>
+        <br />
+        <small>
+          <a href="https://www.openstreetmap.org/#map=6/9.936/8.196">
+            View Larger Map
+          </a>
+        </small>
       </div>
     </main>
   );
