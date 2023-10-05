4b9350b867f64af71f3460faca62c8c7824e32c8
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-10-05 20:40:56 +0000

    s

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 import { useEffect, useRef } from 'react';
 import nigeriaBorder from "../nigeriaBorder.json"
+import L from "leaflet"
 export default function Home() {
-  const mapRef = useRef(null);
+  const mapRef = useRef();
 
   useEffect(() => {
     if (typeof window !== "undefined") {
      // let L = require('leaflet');
-      let map = L.map(mapRef.current).setView([9.082, 8.6753], 6);
+      let map = L.map('map').setView([9.082, 8.6753], 6);
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
