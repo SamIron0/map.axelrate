6bc935e290c28a0acbe79e63b92fa2fa4e3ba39f
sam <ironkwec@myumanitoba.ca>
2023-10-06 16:43:11 -0500

    df

--- app/HomePage/page.tsx
+++ app/HomePage/page.tsx
@@ -13,11 +13,13 @@ const HomePage: React.FC = () => {
       <Map
         mapId="MapTest"
         lat={9.0820}
-        lng={8.6753} 
+        lng={8.6753}
         zoom={5}
-        height="600px"
+        height="850px"
         markers={mapMarkers}
       />
+
+<button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">News</button>
 
 
     </Container>
