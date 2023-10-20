789040ea0385e906d02ee83e0f2826557371c777
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-10-20 11:55:41 -0500

    df

--- app/HomePage/page.tsx
+++ app/HomePage/page.tsx
@@ -12,6 +12,10 @@ const HomePage: React.FC = () => {
   const [zoom, setZoom] = useState(5);
   const [region, setRegion] = useState("");
   const [locationFetched, setLocationFetched] = useState(false); // New state variable
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+
+  Modal.setAppElement('#root');
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -48,27 +52,60 @@ const HomePage: React.FC = () => {
     }
   }, [locationFetched]);
 
+
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   console.log(region);
+
   return (
-    <Container>
-
-      <Map
-        mapId="MapTest"
-        lat={9.0820}
-        lng={8.6753}
-        zoom={zoom}
-        height={height}
-        markers={mapMarkers}
-      />
-      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Post
-        <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-        </svg>
-      </button>
+    <>
 
 
-    </Container>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={{
+          content: {
+            width: '70%',
+            height: '70%',
+            margin: 'auto',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}
+      >
+        <video controls>
+          <source src="YOUR_VIDEO_SOURCE" type="video/mp4" />
+        </video>
+      </Modal>
+      <Container>
+
+        <Map
+          mapId="MapTest"
+          lat={9.0820}
+          lng={8.6753}
+          zoom={zoom}
+          height={height}
+          markers={mapMarkers}
+        />
+        <button onClick={openModal} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Post
+          <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+          </svg>
+        </button>
+
+
+      </Container>
+    </>
   );
 };
 
