9083ea211c278d5485622eca6506ebe6bc74178d
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-11-20 21:19:23 +0000

    j

--- app/HomePage/page.tsx
+++ app/HomePage/page.tsx
@@ -91,9 +91,7 @@ const HomePage: React.FC = () => {
             capture="user"
             onChange={(event: ChangeEvent<HTMLInputElement>) => handleCapture(event.target)}
             className="text-black bg-white mt-3 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center "
-          >
-            POST
-          </input>
+          />
 
         </div>
 
