0f1e98b70170b5a33a6b9fce85bb814543b01771
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-11-20 19:56:12 +0000

    s

--- app/CameraPage/page.tsx
+++ app/CameraPage/page.tsx
@@ -2,33 +2,33 @@
 import React, { useRef } from 'react';
 import { Video, CaptureButton } from './styles';
 
+const App: React.FC = () => {
 
 
-const videoStream = useRef<HTMLVideoElement>(null);
+  const videoStream = useRef<HTMLVideoElement>(null);
 
-const startCapture = async () => {
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      if (videoStream.current) {
-        videoStream.current.srcObject = stream;
-        videoStream.current.play();
+  const startCapture = async () => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoStream.current) {
+          videoStream.current.srcObject = stream;
+          videoStream.current.play();
+        }
+      } catch (error) {
+        console.error("Error accessing media devices.", error);
       }
-    } catch (error) {
-      console.error("Error accessing media devices.", error);
     }
   }
-}
 
 
-const App: React.FC = () => {
-    return (
-      
-        <div className="video-container">
-            <Video/>
-            <CaptureButton />
-        </div>
-    );
+  return (
+
+    <div className="video-container">
+      <Video />
+      <CaptureButton />
+    </div>
+  );
 };
 
 export default App;
