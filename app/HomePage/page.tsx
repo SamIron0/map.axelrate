75b848a4d7b9490053d145ea52a28cc00df71c58
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-11-20 20:25:09 +0000

    s

--- app/HomePage/page.tsx
+++ app/HomePage/page.tsx
@@ -72,29 +72,35 @@ const HomePage: React.FC = () => {
 
   return (
     <div id="root">
-      <Container>
-
-        <Map
-          mapId="MapTest"
-          lat={9.0820}
-          lng={8.6753}
-          zoom={zoom}
-          height={height}
-          markers={mapMarkers}
-        />
-        <Link
-          href="/CameraPage"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      <div
+       className='w-95%'>
+        <div
+          className='w-full rounded-md'>
+          <Map
+            mapId="MapTest"
+            lat={9.0820}
+            lng={8.6753}
+            zoom={zoom}
+            height={height}
+            markers={mapMarkers}
+          />
+        </div>
+        <div
+          className="w-full"
         >
-          POST
-          <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-          </svg>
-        </Link>
+          <Link
+            href="/CameraPage"
+            className="text-black bg-white mt-3 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            POST
+            <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+          </Link>
+        </div>
 
 
-
-      </Container>
+      </div>
 
     </div>
   );
